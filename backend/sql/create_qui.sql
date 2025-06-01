50f864d6a3bb52e33cfcfb4dfe83242788c0282c
Frédéric SCHOEN <102286935+fredschoen@users.noreply.github.com>
2025-06-01 22:16:04 +0200

    import qui

--- backend/sql/create_qui.sql
+++ backend/sql/create_qui.sql
@@ -1,15 +1,25 @@
--- drop TABLE public.qui;
-CREATE TABLE public.qui (
-id INT,
-nom VARCHAR(50),
-prenom VARCHAR(50),
-fullName VARCHAR(50),
-genre VARCHAR(1),
-domaine VARCHAR(50),
-style VARCHAR(50),
-remarque VARCHAR(1000),
-createTS VARCHAR(20),
-modifTS VARCHAR(20),
-dateNaiss VARCHAR(10),
-dateDeces VARCHAR(10)
-);
+-- Table: public.qui
+
+DROP TABLE IF EXISTS public.qui;
+
+CREATE TABLE IF NOT EXISTS public.qui
+(
+    id integer,
+    nom character varying(50) COLLATE pg_catalog."default",
+    prenom character varying(50) COLLATE pg_catalog."default",
+    fullname character varying(50) COLLATE pg_catalog."default",
+    genre character varying(1) COLLATE pg_catalog."default",
+    domaine character varying(50) COLLATE pg_catalog."default",
+    pays character varying(50) COLLATE pg_catalog."default",
+    style character varying(50) COLLATE pg_catalog."default",
+    remarque character varying(1000) COLLATE pg_catalog."default",
+    creatts character varying(20) COLLATE pg_catalog."default",
+    modifts character varying(20) COLLATE pg_catalog."default",
+    datenaiss character varying(10) COLLATE pg_catalog."default",
+    datedeces character varying(10) COLLATE pg_catalog."default"
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.qui
+    OWNER to postgres;
