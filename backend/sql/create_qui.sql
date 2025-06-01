88eec740239fc1e1f1317b7ea83ff55e3b52b9dd
Frédéric SCHOEN <102286935+fredschoen@users.noreply.github.com>
2025-06-01 13:25:37 +0200

    Create create_qui.sql

---
+++ backend/sql/create_qui.sql
@@ -0,0 +1,15 @@
+-- drop TABLE public.qui;
+CREATE TABLE public.qui (
+id INT,
+nom VARCHAR(50),
+prenom VARCHAR(50),
+fullName VARCHAR(50),
+genre VARCHAR(1),
+domaine VARCHAR(50),
+style VARCHAR(50),
+remarque VARCHAR(1000),
+createTS VARCHAR(20),
+modifTS VARCHAR(20),
+dateNaiss VARCHAR(10),
+dateDeces VARCHAR(10)
+);
