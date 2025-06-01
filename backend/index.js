6b60d73373101ae32343bdabc0aa5e66900dfc05
Frédéric SCHOEN <102286935+fredschoen@users.noreply.github.com>
2025-06-01 09:13:30 +0200

    init

---
+++ backend/index.js
@@ -0,0 +1,39 @@
+const express=require('express')
+const app=express()
+app.use(express.json())
+
+const {Client}=require('pg')
+const con=new Client({
+    host:"localhost",
+    user:"postgres",
+    password:"fred",
+    port:5432,
+    database:"quiquoitekdb"
+})
+
+
+con.connect().then(()=> console.log("connected"))
+
+app.get('/coucou',(req,res)=>{
+    console.log("coucou")
+    res.send("get coucou")
+})
+
+app.post('/postData',(req,res)=>{
+    console.log("/postData")
+    const {nom,id}=req.body
+    const insert_query="INSERT INTO qui (nom;id) VALUES ($1,$2)"
+    con.query(insert_query,[nom,id],(err,result)=>{
+        if(err) {
+            res.end(err)
+        } else{
+            console.log(result)
+            res.send("POSTED DATA")
+        }
+
+    })
+})
+
+app.listen(3000,()=>{
+console.log("server is running...")
+})
